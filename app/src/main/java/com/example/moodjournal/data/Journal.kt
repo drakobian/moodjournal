3ab5f8e05f7764a9236300ab30308ab472e23821
drakobian <drakebennion@gmail.com>
2023-11-02 17:15:52 -0600

    add journal and dao

---
+++ app/src/main/java/com/example/moodjournal/data/Journal.kt
@@ -0,0 +1,13 @@
+package com.example.moodjournal.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "journals")
+data class Journal(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val event: String,
+    val date: LocalDate,
+)
---
+++ app/src/main/java/com/example/moodjournal/data/JournalDao.kt
@@ -0,0 +1,27 @@
+package com.example.moodjournal.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface JournalDao {
+    @Query("SELECT * FROM journals WHERE id = :id")
+    fun getJournal(id: Int): Flow<Journal>
+
+    @Query("SELECT * FROM journals ORDER BY date DESC")
+    fun getAllJournals(): Flow<List<Journal>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(journal: Journal)
+
+    @Update
+    suspend fun update(journal: Journal)
+
+    @Delete
+    suspend fun delete(journal: Journal)
+}
